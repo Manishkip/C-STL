914cc2ec8866c5dfc31d8da565d6a75b437f3c8c
Manishkip <89375969+Manishkip@users.noreply.github.com>
2022-03-10 19:24:50 +0530

    Create queue.cpp

---
+++ queue.cpp
@@ -0,0 +1,22 @@
+// Queue functions in stl
+#include <iostream>
+#include<queue>
+using namespace std;
+
+int main() {
+   
+    queue<string> q; 
+    q.push("Love");
+    q.push("Babbar");
+    q.push("Kumar");
+    
+    cout<<"Size before pop -> "<<q.size()<<endl;
+    
+    cout<<"First element-> "<<q.front()<<endl;
+    q.pop();
+    cout<<"First element -> "<<q.front()<<endl;
+    cout<<"Size after pop -> "<<q.size()<<endl;
+    cout<<q.back();
+
+    return 0;
+}
